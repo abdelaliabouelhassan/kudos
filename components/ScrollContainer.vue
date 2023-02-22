6e8807cd2f3a85b99868c6315674b133afc261a0
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2023-02-22 17:13:48 +0100

    desktop

---
+++ components/ScrollContainer.vue
@@ -0,0 +1,146 @@
+<template>
+  <div class="w-full">
+   <div class=" w-full h-full max-w-[884px] max-h-[133px]">
+    <BrowserHeader />
+
+   </div>
+
+    <div class=" w-full max-h-[477.07px] overflow-y-auto h-full   max-w-[920px]  space-y-10">
+       <div class="w-full bg-[#050608] max-w-[884px] space-y-10">
+         <div class=" w-full flex flex-col items-center space-y-8 py-10 px-14" id="first">
+            <div class=" w-full flex items-start justify-between">
+                <div class=" w-full h-full  max-w-[165px] max-h-[202px]">
+                    <img src="/images/box.png" class=" w-full h-full" alt="">
+                </div>
+                 <div class=" w-full h-full max-w-[165px] max-h-[202px]">
+                    <img src="/images/nike.png" class=" w-full h-full" alt="">
+                </div>
+                 <div class=" w-full h-full max-w-[165px] max-h-[202px]">
+                    <img src="/images/csk.png" class=" w-full h-full" alt="">
+                </div>
+            </div>
+            <div class=" w-full flex items-center space-x-5 bg-black border border-[#FC9D0F] rounded-lg py-2 px-8 max-w-[644px]" style="box-shadow: 0px 8px 8px 8px rgba(0, 0, 0, 0.25);">
+              <div>
+                <img src="/images/key.png" alt="">
+              </div>
+              <span class=" text-2xl font-black font-montserrat italic text-white">Unlock <span class=" text-[#FC9D0F]">your</span> shopping freedom</span>
+            </div>
+         </div>
+          <div class=" w-full flex flex-col items-center space-y-2 py-10 px-14" id="second">
+            <div class="">
+                <div class=" w-full h-full  max-w-[240px] max-h-[240px]">
+                    <img src="/images/cump.png" class=" w-full h-full" alt="">
+                </div>
+            </div>
+            <div class=" w-full flex items-center space-x-5 bg-black border border-[#FC9D0F] rounded-lg py-2 px-8 max-w-[644px]" style="box-shadow: 0px 8px 8px 8px rgba(0, 0, 0, 0.25);">
+              <div>
+                <img src="/images/bgc.png" class=" w-[80px] h-[80px]" alt="">
+              </div>
+              <span class=" text-2xl font-black font-montserrat italic text-white">Rewarding <span class=" text-[#FC9D0F]">every</span>purchase</span>
+            </div>
+         </div>
+         <div class=" w-full flex flex-col items-center space-y-2 py-10 px-14" id="third">
+            <div class="">
+                <div class=" w-full h-full  max-w-[240px] max-h-[240px]">
+                    <img src="/images/lbox.png" class=" w-full h-full" alt="">
+                </div>
+            </div>
+            <div class=" w-full flex items-center space-x-5 bg-black border border-[#FC9D0F] rounded-lg py-2 px-8 max-w-[644px]" style="box-shadow: 0px 8px 8px 8px rgba(0, 0, 0, 0.25);">
+              <div>
+                <img src="/images/book.png" class=" w-[80px] h-[80px]" alt="">
+              </div>
+              <span class=" text-2xl font-black font-montserrat italic text-white">Shop <span class=" text-[#FC9D0F]">smarter</span>, not harder</span>
+            </div>
+         </div>
+       </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import BrowserHeader from '~/components/BrowserHeader.vue'
+export default {
+  data(){
+    return {
+      active: false,
+    }
+  },
+    components: {
+        BrowserHeader
+    },
+    methods:{
+      ScrollTo(id){
+        document.getElementById(id).scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+         this.active = false
+        let time =   setTimeout(() => {
+            this.active = true
+            clearTimeout(time)
+          }, 100);
+        
+      }
+    },
+    mounted(){
+      const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.5
+      }
+      const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            if(entry.target.id == 'first'){
+             if(this.active){
+               this.$emit('scroll', 'first')
+             }
+            }
+            if(entry.target.id == 'second'){
+              if(this.active){
+                this.$emit('scroll', 'second')
+              }
+            }
+            if(entry.target.id == 'third'){
+               if(this.active){
+                this.$emit('scroll', 'third')
+               }
+            }
+          }
+        })
+      }, options)
+      observer.observe(document.querySelector('#first'))
+      observer.observe(document.querySelector('#second'))
+      observer.observe(document.querySelector('#third'))
+      setTimeout(() => {
+        this.active = true
+      }, 100);
+
+
+    }
+    
+}
+</script>
+
+
+
+<style scoped>
+::-webkit-scrollbar {
+  width: 10px;
+  border-radius: 10px;
+}
+::-webkit-scrollbar-track {
+  background: #D9D9D9;
+    border-radius: 10px;
+
+}
+::-webkit-scrollbar-thumb {
+  background: #FC9D0F;
+    border-radius: 10px;
+
+}
+::-webkit-scrollbar-thumb:hover {
+  background: #e29d35;
+    border-radius: 10px;
+
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,120 @@
+<template>
+    <div class=" w-full h-full  bg-[#1F1E1E]" style="
+            background-image: url('/images/background.png');
+            background-repeat: no-repeat;
+            background-size: cover;
+            backdrop-filter: blur(2px);
+        ">
+       <!-- Header -->
+         <Header />
+       <!-- Content -->
+       
+        <div class=" w-full h-full  bg-[#1F1E1E] bg-opacity-90 px-4" >
+        <!-- First Section -->
+
+           <section class=" w-full max-w-[1568px] m-auto flex items-center justify-between pt-10">
+            <div class=" w-full max-w-[530.38px] flex flex-col items-start space-y-6">
+                <div>
+                    <span class=" text-white font-extrabold font-montserrat text-[70px] italic leading-[63px]">BUY ONE,</span><br>
+                    <span class=" text-white font-extrabold font-montserrat text-[70px] italic leading-[63px]"> GET ONE...</span> <br>
+                    <span class="text-[#FC9D0F] font-black font-anybody text-[70px] leading-[63px]">EVERYTIME</span>
+                </div>
+            <button class=" flex items-center space-x-4 rounded-full py-2 px-6 bg-[#FC9D0F] ring-[1px] ring-white hover:bg-opacity-70 hover:ring-0">
+                <span class=" text-white text-xl font-poppins font-medium">Download the Extension </span>
+                <DownloadIcon />
+            </button>
+            </div>
+            <div class=" w-full max-w-[856px]">
+                <img src="/images/cntrls.svg" class=" w-full h-full" alt="">
+            </div>
+           </section>
+
+        <!-- Second Section -->
+
+        <section class=" w-full h-full pt-10">
+            <div class=" w-full max-w-[1731px] py-40  m-auto">
+                 <div class=" w-full flex items-start">
+                    <div class=" col-span-2 w-full max-w-[920px]">
+                        <ScrollContainer @scroll="scroll"  />
+                    </div>
+                    <div class=" w-full max-w-[817px]">
+                        <span class=" text-white font-black font-montserrat italic text-[32px]">HOW IT WORKS</span>
+                        <div class=" w-full px-10 pt-16 flex flex-col items-start space-y-10">
+                            <div @click="setActive(0)" :class="{'px-10 py-5 w-full border-[3px] border-[#FC9D0F] bg-black': activeIndex === 0,'px-4 py-2 bg-[#7B7D7F] rounded-lg': activeIndex != 0}">
+                                <span class=" font-bold text-white font-montserrat text-[26px] whitespace-nowrap">An experience created by you.</span>
+                                <div class=" text-white text-base font-montserrat font-normal pl-5" v-if="activeIndex === 0">
+                                    Stores and rewards based on your choice.
+                                </div>
+                            </div>
+                              <div @click="setActive(1)" :class="{'px-10 py-5 w-full border-[3px] border-[#FC9D0F] bg-black': activeIndex === 1,'px-4 py-2 bg-[#7B7D7F] rounded-lg': activeIndex != 1}">
+                                <span class=" font-bold text-white font-montserrat text-[26px] whitespace-nowrap">Don't waste time by waiting for the right time.</span>
+                                <div class=" text-white text-base font-montserrat font-normal pl-5" v-if="activeIndex === 1">
+                                    No more saving products for later or waiting for price drops, If you love it, buy it!
+                                </div>
+                            </div>
+                             <div @click="setActive(2)" :class="{'px-10 py-5 w-full border-[3px] border-[#FC9D0F] bg-black': activeIndex === 2,'px-4 py-2 bg-[#7B7D7F] rounded-lg': activeIndex != 2}">
+                                <span class=" font-bold text-white font-montserrat text-[26px] whitespace-nowrap">With Kudos, every purchase is a joyful experience.</span>
+                                <div class=" text-white text-base font-montserrat font-normal pl-5" v-if="activeIndex === 2">
+                                    Shop smarter, shop with kudos!
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                 </div>
+            </div>
+        </section>
+
+        <!-- Third Section -->
+
+        <section class=" w-full flex flex-col items-center  pt-10">
+            <div class=" flex flex-col items-start space-y-4">
+                <p class=" max-w-[679px] text-white font-black font-montserrat italic text-[50px] leading-[45px]">
+                    <span class=" text-[#FC9D0F]">BUY</span> PRODUCTS YOU LOVE, <span class="text-[#FC9D0F]">REDEEM</span> PRODUCTS TO <span class="text-[#FC9D0F]">BRAG</span> ABOUT.
+                </p>
+                <button class=" flex items-center space-x-4 rounded-full py-2 px-6 bg-[#FC9D0F] ring-[1px] ring-white hover:bg-opacity-70 hover:ring-0">
+                    <span class=" text-white text-xl font-poppins font-medium">Download the Extension </span>
+                    <DownloadIcon />
+                </button>
+            </div>
+            <div class=" text-white text-[50px] font-black font-montserrat italic text-center pb-10 pt-40">
+                <span class=" text-[#FC9D0F]">xxxx users </span> have received rewards
+            </div>
+        </section>
+
+          
+        </div>
+       
+    </div>
+</template>
+
+<script>
+import Header from '~/components/Header.vue'
+import DownloadIcon from '~/components/icons/DownloadIcon.vue'
+import ScrollContainer from '~/components/ScrollContainer.vue'
+export default {
+    components: {
+        Header,
+        DownloadIcon,
+        ScrollContainer
+    },
+    data(){
+        return{
+            activeIndex:0,
+        }
+    },
+    methods:{
+        scroll(e){
+            if(e == 'first'){
+                this.activeIndex = 0
+            }else if(e == 'second'){
+                this.activeIndex = 1
+            }
+            else if(e == 'third'){
+                this.activeIndex = 2
+            }
+        },
+    }
+   
+    
+}
+</script>
